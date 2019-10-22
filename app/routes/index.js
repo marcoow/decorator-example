81e90f6249f2752e43f9eac9178be206d9605ebe
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-10-22 15:26:14 +0200

    time model method

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,25 @@
 import Route from '@ember/routing/route';
 
+function timed(target, name, descriptor) {
+  let { value: original } = descriptor;
+  if (typeof original === 'function') {
+    descriptor.value = function(...args) {
+      let startTime = performance.now();
+
+      let result = original.apply(this, args);
+
+      let endTime = performance.now();
+      let duration = endTime - startTime;
+      console.log(`⏱ ${name}: ${duration}s.`);
+
+      return result;
+    }
+  }
+  return descriptor;
+}
+
 export default Route.extend({
+  @timed
   async model() {
     let delay = 1 + (Math.random() * 1000);
     
